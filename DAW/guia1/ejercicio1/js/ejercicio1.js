19131e617ae4b950ba1edef21e2dec313788ea47
Carlos <71299504+NoobMaster-69-UwU@users.noreply.github.com>
2022-07-08 09:33:09 -0600

    Add files via upload

---
+++ DAW/guia1/ejercicio1/js/ejercicio1.js
@@ -0,0 +1,12 @@
+
+document.write("<header>Header</header>");
+
+document.write("<div class='cont2'>");
+document.write("<aside>Navigation</aside>");
+document.write("<div class='art'>");
+document.write("<article>Article</article>");
+document.write("<article class='arti'>Article</article>");
+document.write("<article>Article</article>");
+document.write("</div>");
+document.write("</div>");
+document.write("<footer>Footer</footer>");
