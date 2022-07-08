ec1f910699fb277dee15716a849913ccf72e7c78
Carlos <71299504+NoobMaster-69-UwU@users.noreply.github.com>
2022-07-08 10:18:04 -0600

    Add files via upload

---
+++ DAW/guia1/ejercicio3/js/ejercicio3.js
@@ -0,0 +1,9 @@
+
+	var celcius = prompt("Ingrese la temperatura en Celcius");
+
+	var far = celcius * 1.8;
+
+	document.write("<h2>" + celcius + " Grados Celcius </h2>\t");
+	document.write("<h2 class='dos'> es igual a " + (far + 32 ) + " Grados Fharenheit</h2>\t");
+
+
