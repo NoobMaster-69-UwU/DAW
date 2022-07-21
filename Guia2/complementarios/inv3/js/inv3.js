28a4d64d7269ed4e3583f47655f5e0517b1752c3
Carlos <71299504+NoobMaster-69-UwU@users.noreply.github.com>
2022-07-20 21:36:23 -0600

    Add files via upload

---
+++ Guia2/complementarios/inv3/js/inv3.js
@@ -0,0 +1,6 @@
+var arreglo = [' Pepe ',' Marcos ',' Rodrigo ',' Dominico '];
+
+
+document.write("<h2>Uso de array.prototype.reverse()</h2>");
+document.write("<h3>Array normal: " + arreglo + "</h3>");
+document.write("<h3>Array utilizando la función: " + arreglo.reverse() + "</h3>");
