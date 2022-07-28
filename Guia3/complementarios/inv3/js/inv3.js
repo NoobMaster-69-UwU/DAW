27887f784ee88149f6ff93e6769e45a5484b597f
Carlos <71299504+NoobMaster-69-UwU@users.noreply.github.com>
2022-07-28 03:27:11 +0200

    Delete Guia3/complementarios directory

--- Guia3/complementarios/inv3/js/inv3.js
+++
@@ -1,7 +0,0 @@
-var arreglo = [' Pepe ',' Marcos ',' Rodrigo ',' Dominico '];
-
-document.write("<div>");
-document.write("<h2>Uso de array.prototype.reverse()</h2>");
-document.write("<h3>Array normal: " + arreglo + "</h3>");
-document.write("<h3>Array utilizando la función: " + arreglo.reverse() + "</h3>");
-document.write("</div>");
